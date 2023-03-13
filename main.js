eb27b1e9b3dcdf403117fb2768c58cf0b8a1cb56
PauDente <pauladente28@gmail.com>
2023-03-13 16:53:37 -0300

    SUBO CAMBIOS

---
+++ main.js
@@ -0,0 +1,7 @@
+let precio = 10
+
+if (precio < 20)  {
+    alert('ALERT! Estas entrando a la mejor página web del mundo.')
+} else if (precio < 100) {
+    alert('ALERT! Estas entrando a la mejor página web del mundo.')
+} 
